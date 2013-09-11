fd7837581a1694a01ca615c0ad021d2cb13c9d12
Matthew Perry <mperry@ecotrust.org>
2013-09-11 07:49:32 -0700

    spa client side auth info

--- angular_example/app/scripts/services/TestFactory.js
+++ angular_example/app/scripts/services/TestFactory.js
@@ -1,13 +1,19 @@
 'use strict';
 
-app.factory('TestFactory', function ($resource) {
-
-  //var apiUrl = '/stuff.json';
+app.factory('TestFactory', function ($resource, $rootScope) {
   var apiUrl = 'http://aklogbook.ecotrust.org/:userId/forms/:formId/api';
+
+  if (!$rootScope.userId || !$rootScope.formId) {
+    alert("Not logged in? check /app/sessions/auth.js \n TODO: redirect to login");
+    return;
+  }
 
   return {
     awesomeStuff : $resource(apiUrl,
-      {userId: "demo", formId: "FRP53_survey2"},
+      // The $rootScope variables are defined in sessions/auth.js 
+      // running behind nginx, this is dynamically served by django
+      // running behind grunt, it's static
+      {userId: $rootScope.userId, formId: $rootScope.formId},
       {
         query: {
           method: 'JSONP',
---
+++ angular_example/app/sessions/auth.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Assume global app variable
+// running behind grunt, this file is static for testing purposes only
+// see the django template in formhub by the same name for the dynamic version
+// that is served behind nginx
+app.run(function($rootScope){
+    $rootScope.userId = 'demo';
+    $rootScope.formId = 'FRP53_survey2';
+});
