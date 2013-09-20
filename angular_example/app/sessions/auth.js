c580914bc925daf7a6bef8af87c37b63b87535ca
Ryan Hodges <rhodges@ecotrust.org>
2013-09-20 10:50:43 -0700

    committing changes for dev to use our formhub server, include crosswalk for fields to forms, and the current form we're testing with

--- angular_example/app/sessions/auth.js
+++ angular_example/app/sessions/auth.js
@@ -7,5 +7,5 @@
 app.run(function($rootScope){
     $rootScope.userId = 'demo';
     $rootScope.formId = 'FRP53_survey2';
-    $rootScope.baseUrl = 'http://localhost'; // no trailing slash
+    $rootScope.baseUrl = 'http://formhub.aklogbook.ecotrust.org'; // no trailing slash
 });
