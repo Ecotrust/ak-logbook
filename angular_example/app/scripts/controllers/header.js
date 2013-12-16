576345d6fa0fb56a0c62eb0d2f4c5bd052d2e783
rhodges <rhodges@ecotrust.org>
2013-12-16 14:53:58 -0800

    give credit where credit's due

--- angular_example/app/scripts/controllers/header.js
+++ angular_example/app/scripts/controllers/header.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// Solution stolen from "myl" at http://stackoverflow.com/questions/16199418/how-do-i-implement-the-bootstrap-navbar-active-class-with-angular-js
+
 app.controller('headCtrl', function ($scope, $location) {
     $scope.isActive = function (viewLocation) { 
         return viewLocation === $location.path();
