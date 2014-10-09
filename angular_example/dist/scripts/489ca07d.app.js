00303dd2d796016921d7c27d689f0c30e18dd84d
rhodges <rhodges@ecotrust.org>
2014-10-09 15:00:23 -0700

    adding dist to repo

---
+++ angular_example/dist/scripts/489ca07d.app.js
@@ -0,0 +1,51 @@
+'use strict';
+var app = angular.module('angularjsGruntExampleApp', [
+    'ngResource',
+    'leaflet-directive',
+    'ngRoute',
+    'ui.bootstrap'
+  ]);
+var checkLoggedin = function ($location, $rootScope) {
+  if (!$rootScope.userId || $rootScope.userId == '') {
+    $rootScope.message = 'You need to log in.';
+    $location.url('/login');
+  }
+};
+app.config([
+  '$routeProvider',
+  function ($routeProvider) {
+    $routeProvider.when('/', {
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl',
+      resolve: { loggedin: checkLoggedin }
+    }).when('/login', {
+      templateUrl: 'views/login.html',
+      controller: 'LoginController'
+    }).when('/permits/:permitId', {
+      templateUrl: 'views/permits.html',
+      controller: 'PermitsCtrl'
+    }).when('/permits', {
+      templateUrl: 'views/permits.html',
+      controller: 'PermitsCtrl'
+    }).when('/yukon_water/:siteId', {
+      templateUrl: 'views/yukon_water.html',
+      controller: 'YukonWaterCtrl'
+    }).when('/yukon_water', {
+      templateUrl: 'views/yukon_water.html',
+      controller: 'YukonWaterCtrl'
+    }).when('/help', {
+      templateUrl: 'views/help.html',
+      controller: 'helpCtrl'
+    }).otherwise({ redirectTo: '/' });
+  }
+]);
+app.run([
+  '$rootScope',
+  function ($rootScope) {
+    if (!$rootScope.baseUrl) {
+      $rootScope.baseUrl = 'http://localhost';
+    }
+    $(document).ready(function () {
+    });
+  }
+]);
